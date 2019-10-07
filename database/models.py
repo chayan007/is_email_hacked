db33a92d88bf75092988007e974f0cdc418bca65
Chayan Datta <chayand@credy.in>
2019-10-07 18:24:02 +0530

    Models created (2)

--- database/models.py
+++ database/models.py
@@ -11,6 +11,8 @@ class Email(models.Model):
 class Hacks(models.Model):
     site = models.CharField(max_length=200)
     description = models.TextField()
+    title = models.CharField(max_length=200)
+    pwn_count = models.PositiveIntegerField(max_length=20)
 
     def __str__(self):
         return self.site
@@ -19,6 +21,8 @@ class Hacks(models.Model):
 class HackMap(models.Model):
     email = models.ForeignKey(Email, on_delete=models.CASCADE)
     hack = models.ForeignKey(Hacks, on_delete=models.CASCADE)
+    fields_hacked = models.TextField(max_length=500)
+    pastes = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
