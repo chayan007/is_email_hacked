41d7802b6a1f1bb14462732d34cea406a069a0a8
Chayan Datta <chayand@credy.in>
2019-10-07 18:15:19 +0530

    Email Validator done

--- validator/views.py
+++ validator/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from .constants import VALIDATOR_URL, REFERER_URL
 import requests
 
 
-def check_if_email_hacked(request, email):
+def get_response(email):
     email.replace('@', '%')
     res = requests.get(url=VALIDATOR_URL + email, data=REFERER_URL)
-    response = res.json()
-    return response
+    return res
 
 
+def check_if_email_hacked(request, email):
+    response = get_response(email)
+    if response.status_code == 404:
+        return JsonResponse({
+            'status': 'safe',
+            'breaches': 0
+        })
+    else:
+        return response.json()
