9e363f54ee9e1b1ab952ec0f55aa8f82603608ef
Chayan Datta <chayand@credy.in>
2019-10-07 18:19:27 +0530

    Models created

--- database/models.py
+++ database/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Email(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
+class Hacks(models.Model):
+    site = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.site
+
+
+class HackMap(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    hack = models.ForeignKey(Hacks, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
