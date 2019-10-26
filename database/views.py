c403baad55675bca4440dfc60ff6421307d32da7
Chayan Datta <chayand@credy.in>
2019-10-27 00:34:36 +0530

    Hack saved to db

--- database/views.py
+++ database/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
 from .handler import *
 
 
 def kickoff_database_tasks(email, res):
     response_json = res.json()
+    hack_dictionary = response_json['Breaches']
     is_email_stored = add_email(email)
-    is_hack_stored = add_hack(response_json['Breaches'])
+    is_hack_stored = add_hack(hack_dictionary)
     if is_hack_stored and is_email_stored:
-        return
+        map_email_hacks(email, hack_dictionary)
     else:
         raise IndexError('Email or Hack was not stored properly')
